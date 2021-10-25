ee3172559bdb8113901da1b81a31660cc2c1875d
SHINSUNGJIN <pxp13716@gmail.com>
2021-10-25 18:06:43 +0900

    .

---
+++ Chap02_JSX/src/App.js
@@ -0,0 +1,20 @@
+
+const elem = <h1>Hello World 01</h1>
+
+function App() {
+  // return elem;
+  return <h1>Hello World 02</h1>
+}
+export default App;
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <h1>Chap02 JSX</h1>
+    </div>
+  );
+}
+
+export default App;
+*/
