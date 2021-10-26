bd18ec77c52859430f30ff9ba42d103c5e6bcd76
SHINSUNGJIN <pxp13716@gmail.com>
2021-10-26 14:59:32 +0900

    .

--- Chap03_ClassComponent/src/App.js
+++ Chap03_ClassComponent/src/App.js
@@ -2,6 +2,7 @@ import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import A01ClassProps from './components/A01ClassProps'
 import A02ClassState from './components/A02ClassState'
+import A03ClassEvent from './components/A03ClassEvent'
 
 function App() {
   const user = { name: 'HungBu', age: 20 };
@@ -12,6 +13,8 @@ function App() {
     <div className="card-body">
       <h1>Chap03 Class Component</h1>
 
+      <A03ClassEvent /><br />
+      
       <A02ClassState /><br />
 
       <A01ClassProps name="NolBu" age={30} user={user} arr={ary} onAdd={onAdd} isChecked >
--- Chap03_ClassComponent/src/components/A03ClassEvent.js
+++ Chap03_ClassComponent/src/components/A03ClassEvent.js
@@ -13,7 +13,33 @@ export class A03ClassEvent extends Component {
         baseball: 'NC',
         four: [],
     };
+
+    sendData = (evt) => {
+        evt.preventDefault();           // DOM이 가지고 있는 내장 JavaScript 기능을 OFF.
+        // Ajax로 서버에 전송
+        console.log(this.state);
+    }
     
+    changeName = (evt) => {
+        console.log(evt.target);        // Event를 발생시킨 DOM 자체를 의미한다.
+        if(evt.target.name === '') alert('값이 없습니다.')
+        this.setState({name: evt.target.value})
+    }
+
+    changeNumber = (evt) => {
+        let value = Number(evt.target.value);
+        if(isNaN(value)) value = 0;
+        this.setState( { [evt.target.name]: value} );
+    }
+    changeString = (evt) => {
+        // 적절한 가드는 구현해야 한디.
+        this.setState( {[evt.target.name]: evt.target.value} );
+    };
+
+    changeCheck = () => {
+        this.setState({isChecked: !this.state.isChecked})
+    }
+
 
     render() {
         return (
@@ -22,29 +48,36 @@ export class A03ClassEvent extends Component {
 
                 <form>
                     Name: {this.state.name}
-                        <input type="text" name="name" className="form-control" />
+                        <input type="text" name="name" className="form-control" 
+                            value={this.state.name} onChange={this.changeName} />
                     Age: {this.state.age} 
-                        <input type="text" name="age" className="form-control" />
+                        <input type="text" name="age" className="form-control" 
+                            value={this.state.age} onChange={this.changeNumber} />
                     Date: {this.state.date}
-                        <input type="date" name="date" className="form-control" />
+                        <input type="date" name="date" className="form-control" 
+                            value={this.state.date} onChange={this.changeString}/>
 
                     RadioButton: {this.state.sports}<br />
                         <div className="form-check">
-                            <input type="radio" name="sports" value="baseball" id="baseball" className="form-check-input" />
+                            <input type="radio" name="sports" value="baseball" id="baseball" className="form-check-input"
+                                 onChange={this.changeString}  defaultChecked={this.state.sports === 'baseball'} />
                             <label htmlFor="baseball" className="form-check-label">야구</label>
                         </div>
                         <div className="form-check">
-                            <input type="radio" name="sports" value="soccer" id="soccer" className="form-check-input" />
+                            <input type="radio" name="sports" value="soccer" id="soccer" className="form-check-input" 
+                                onChange={this.changeString}  defaultChecked={this.state.sports === 'soccer'} />
                             <label htmlFor="soccer" className="form-check-label">축구</label>
                         </div>
                         <div className="form-check">
-                            <input type="radio" name="sports" value="basketball" id="basketball" className="form-check-input" />
+                            <input type="radio" name="sports" value="basketball" id="basketball" className="form-check-input" 
+                                onChange={this.changeString}  defaultChecked={this.state.sports === 'basketball'} />
                             <label htmlFor="basketball" className="form-check-label">농구</label>
                         </div>
 
-                    CheckBox One: {this.state.isChecked}<br />
+                    CheckBox One: {this.state.isChecked ? '동의' : '동의 안함'}<br />
                         <div className="form-check">
-                            <input type="checkbox" id="check" className="form-check-input" />
+                            <input type="checkbox" id="check" className="form-check-input" 
+                                value={this.state.isChecked} onChange={this.changeCheck} />
                             <label htmlFor="check" className="form-check-label">동의</label>
                         </div>
 
@@ -63,7 +96,8 @@ export class A03ClassEvent extends Component {
                         </div>
 
                     SelectBox: {this.state.baseball}<br/>
-                        <select name="baseball" className="form-control" >
+                        <select name="baseball" className="form-control"
+                            value={this.state.baseball} onChange={this.changeString} >
                             <option>NC</option>
                             <option>두산</option>
                             <option>엘지</option>
@@ -77,7 +111,7 @@ export class A03ClassEvent extends Component {
                         </select>
                     <br />
 
-                    <button type="submit">SEND</button>
+                    <button type="submit" onClick={this.sendData}>SEND</button>
                 </form>
             </div>
         )
