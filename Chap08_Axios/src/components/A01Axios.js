fdbd0e30ef67885d6bcdd06bc903d14aee87fcda
SHINSUNGJIN <pxp13716@gmail.com>
2021-10-29 08:59:57 +0900

    .

--- Chap08_Axios/src/components/A01Axios.js
+++ Chap08_Axios/src/components/A01Axios.js
@@ -6,16 +6,16 @@ function A01Axios() {
     const baseURL = 'http://localhost:8080/contacts/';
     const [data, setData] = useState();
 
-    const getContactList = (pageno=1, pagesize=10) => {
-        axios.get(baseURL, {params: {pageno: 1, pagesize: 10}})
+    const getContactList = (pageno, pagesize) => {
+        axios.get(baseURL, {params: {pageno: pageno || 1, pagesize: pagesize || 10}})
         .then( resp => setData(resp.data))
         .catch( error => setData(error))
     };
 
     // await를 실행하는 함수는 async가 필수. 비 동기를 동기화 해서 실행할 목적
-    const getContactListAsync = async (pageno=1, pagesize=10) => {
+    const getContactListAsync = async (pageno, pagesize) => {
         try {
-            const resp = await axios.get(baseURL, {params: {pageno: 1, pagesize}});  // 결과값이 대입될때까지 아래는 실행 안됨
+            const resp = await axios.get(baseURL, {params: {pageno: pageno || 1, pagesize: pagesize || 10}});  // 결과값이 대입될때까지 아래는 실행 안됨
             setData(resp.data);
 
             // data에 값이 들어온 후에 실행된다
@@ -67,8 +67,8 @@ function A01Axios() {
     return (
         <div>
             <h3>A01 Axios GET</h3>
-            <button onClick={getContactList}>DATA LIST</button>
-            <button onClick={getContactListAsync}>DATA LIST ASYNC</button>
+            <button onClick={() => getContactList()}>DATA LIST</button>
+            <button onClick={() => getContactListAsync()}>DATA LIST ASYNC</button>
             <button onClick={ () => getContact(50) }>GET</button>
             <button onClick={addContact}>ADD</button>
             <button onClick={() => updateContact(1635405651905)}>UPDATE</button>
