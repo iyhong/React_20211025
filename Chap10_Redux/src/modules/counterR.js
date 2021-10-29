d363ff4068632b607364a80649faa9e289f7a2fe
SHINSUNGJIN <pxp13716@gmail.com>
2021-10-29 10:50:23 +0900

    .

---
+++ Chap10_Redux/src/modules/counterR.js
@@ -0,0 +1,13 @@
+
+const init = {
+    title: 'CounterR',
+    num: 0,
+}
+const counterR = (state = init, action) => {
+    switch(action.type) {
+        
+        default: 
+            return state;
+    }
+}
+export default counterR;
